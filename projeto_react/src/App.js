5ad2d5dee19f11b0457df13f5fca8b4f052f950e
Renan Victorino <renan.gvg@hotmail.com>
2025-08-11 21:50:48 -0300

    cesta

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -3,29 +3,18 @@ import './App.css';
 
 function App() { // aqui é JavaScript
 
-
-  function calculadora (a, b, op){
-   switch(op){
-    case '+':
-      return a + b 
-    case '-':
-      return a - b 
-    case '*':
-      return a * b 
-    case '/':
-      return a / b 
-   }
+  let cesta = ['esfirra', 'pavê', 'pão de queijo', 'coca-cola'];
   
-  }
+  let desenha = [];
+
+    for(let i= 0; i < 3; i++  ){
+      desenha.push(<p> {cesta[i]} </p>)
+}
 
   return ( // Aqui é html
     <main className="App">
-    {(calculadora(8, 5, '/')).toFixed(1)}<br/>
-    {calculadora(8, 5, '*')}<br/>
-    {calculadora(8, 5, '-')}<br/>
-    {calculadora(8, 5, '+')}
-    {calculadora(8, calculadora(8, 5, '*'), '+')}
-    
+      {/* {cesta.join(', ')} */}
+     {desenha}
     </main>
   );
 }
