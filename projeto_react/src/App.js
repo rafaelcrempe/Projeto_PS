cab729d7b3623075b0b19796bcf6ec41d9ce5193
marcoshenriquecurila <mhcurila@gmail.com>
2025-08-11 21:18:20 -0300

    switch feito

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -4,24 +4,22 @@ import './App.css';
 function App() {  // aqui é java script
   
   function calculadora( a, b, op){
+    switch(op){
+    case '+' :
+      return a+b
+    case "-" :
+      return a-b
+    case '*' :
+      return a*b
+    case '/' :
+      return a/b
 
-    if(op == "+"){
-      return a + b
-
-    } else if( op == "-"){
-      return a - b
-
-    } else if( op == "*"){
-      return a * b
-
-    } else if( op == "/"){
-      return a/ b
     }
   } 
 
   return ( /* aqui é  html */
     <main className="App">
-      {calculadora(8, calculadora(4, 5, "*"),  "/")}<br/>
+      {calculadora(8, calculadora(4, 5, "*"),  "/") }<br/>
       {calculadora( 12, 4, "/")}<br/>
       {calculadora( 36, 12, "*")}<br/>
     </main>
