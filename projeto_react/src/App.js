da25dcdfafaa9493deff12a68d42002746c41a53
Rafael Crempe <rafaelcrempe@gmail.com>
2025-08-11 21:17:53 -0300

    switch case

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -1,29 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() { 
+function App() {
 
-  function calculadora(a, b, op){
-    if(op == '+'){
-      return a + b;
-    } else if(op == '-'){
-      return a - b;
-    } else if(op == '*'){
-      return a * b;
-    } else if(op == '/') {
-      return a / b;
-    } 
+  function calculadora(a, b, op) {
+
+    switch (op) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return a / b;
+    }
   }
 
 
   return (
     <main className="App">
-   {calculadora(2,5,'+')} <br/>
-   {calculadora(10,10,'-')} <br/>
-   {calculadora(10,3,'*')} <br/>
-   {(calculadora(10,3,'/').toFixed(1))} <br/>
-   {calculadora(5,calculadora(8,7,'*'),'+')} <br/>
-    
+      {calculadora(2, 5, '+')} <br />
+      {calculadora(10, 10, '-')} <br />
+      {calculadora(10, 3, '*')} <br />
+      {(calculadora(10, 3, '/').toFixed(1))} <br />
+      {calculadora(5, calculadora(8, 7, '*'), '+')} <br />
+
     </main>
   );
 }
