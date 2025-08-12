4eaf01aee135e9359dc2ad89f29ceb5791e2dcd9
marcoshenriquecurila <mhcurila@gmail.com>
2025-08-11 22:29:56 -0300

    lista impares

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -3,26 +3,24 @@ import './App.css';
 
 function App() {  // aqui é java script
   
-  let cesta = ['pao', 'suco', 'chocolate', 'torrada']
-  let desenha = [];
+  // faça uma lista com todos os numeros  até 10
+  // mas exiba apenas os números impares
 
-  // declara o indice; compara se é para continuar; incrementa o indice 
-  for( let i=0;  i<3; i++ ){
-      desenha.push(<p>{ cesta[i]} </p>)
+  let lista = [0,1,2,3,4,5,6,7,8,9,10] 
+  let impares = []
+
+  for(let i = 0; i < lista.length; i++){
+    if(lista[i] % 2 != 0) {
+      impares.push(lista[i])
+    }
+    
+
   }
-  
-
   return ( /* aqui é  html */
     <main className="App">
       
-     {/* {cesta.join( ', ')} */}
-     
-      {/* {cesta} */}
+    {impares}
 
-      {cesta[2]}
-
-      
-     
       
     </main>
   );
