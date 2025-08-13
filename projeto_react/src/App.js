e671f610c86ebdcba85e1c0285daeec5b787c0bf
Renan Victorino <renan.gvg@hotmail.com>
2025-08-12 22:08:05 -0300

    revisao html, css e controle de variaveis

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -3,25 +3,58 @@ import './App.css';
 
 function App() { // aqui é JavaScript
 
- // Faça um lista com todos os numeros mas exiba apenas o numeros 
- // impares
+  let email = ""
 
- let lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  function mudaEmail(valor){
+    email = valor
+  }
 
- let impares = [];
+  let senha = ""
 
- for(let i= 0; i < lista.length; i++){
-    if(lista[i] % 2 == 1){
-      impares.push(<p>{lista[i]}</p>);
-    }
+  function mudarSenha(valor){
+    senha = valor
+  }
 
- }
+  function enviar(){
+    alert(`Email: ${email}\nSenha: ${senha}`)
+  }
+
+  let isLogin = true;
 
   return ( // Aqui é html
     <main className="App">
-   
-   {impares}
-    
+      
+      <h1>Entrar</h1>
+
+      <button onClick={() => {isLogin = !isLogin}} >
+        {isLogin && ("Cadastrar-se")}
+        {!isLogin && ("Voltar para o login")}
+      </button>
+
+      {!isLogin && (
+      <form className="register" >
+        
+      </form>
+      )}
+
+      {isLogin && (
+      <form className="login" >
+
+        <label>
+          Email: <input name="Email" type="email" placeholder="Digite o Email" onChange={(e) => mudaEmail(e.target.value)} /><br/>
+        </label>
+
+        <label>
+          Senha: <input name="Senha" type="password" placeholder="Digite a senha" onChange={(e) => mudarSenha(e.target.value)} /><br/>
+        </label>
+
+      <button className="buttonSuccess" onClick={() => enviar()} >Login</button>
+
+      <p>Não Tem cadastro? <a href="#">Clique aqui</a></p>
+
+      </form>
+      )}
+
     </main>
   );
 }
