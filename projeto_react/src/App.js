02d7302c13c8610b5ee043b8f53a8473f6a72b0e
Rafael Crempe <rafaelcrempe@gmail.com>
2025-08-12 22:07:31 -0300

    cadastro de usuário

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -3,25 +3,59 @@ import './App.css';
 
 function App() { // aqui é JavaScript
 
- // Faça um lista com todos os numeros mas exiba apenas o numeros 
- // impares
+  let email = ""
+  let senha = ""
 
- let lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  function mudarEmail(valor) {
+    email = valor
+  }
 
- let impares = [];
+  function mudarSenha(valor) {
+    senha = valor
+  }
 
- for(let i= 0; i < lista.length; i++){
-    if(lista[i] % 2 == 1){
-      impares.push(<p>{lista[i]}</p>);
-    }
+  function enviar() {
+    alert(`Email: ${email}\nSenha: ${senha}`)
+  }
 
- }
+
+  let isLogin = true;
 
   return ( // Aqui é html
     <main className="App">
-   
-   {impares}
-    
+
+    <button onClick={() => {isLogin = !isLogin}}>
+      {isLogin && ("Cadastre-se")}
+      {!isLogin && ("Voltar para o login")}
+    </button>
+
+      {!isLogin &&(
+        <form className="register">
+           <input type="text" placeholder="Nome"/><br/>
+            <input type="text" placeholder="Sobrenome"/><br/>
+            <input type="date" placeholder="Data Nascimento"/><br/>
+            <input type="text" placeholder="CPF"/><br/>
+            <input type="tel" placeholder="Telefone"/><br/>
+            <input type="email" placeholder="E-mail"/><br/>
+            <input type="password" placeholder="Senha"/><br/>
+            <input type="password" placeholder="Confirmar Senha"/><br/>
+        </form>
+      )}
+
+      {isLogin && (
+        <form className="login">
+          <label>
+            Email: <input name="Email" type="email" placeholder="Digite o seu email" onChange={(e) => mudarEmail(e.target.value)} /><br />
+          </label>
+          <label>
+            Senha: <input name="Senha" type="password" placeholder="Digite a sua senha" onChange={(e) => mudarSenha(e.target.value)} /><br />
+          </label>
+
+          <button className="buttonSuccess" onClick={() => enviar()}>Entrar!</button>
+        </form>
+      )}
+
+
     </main>
   );
 }
