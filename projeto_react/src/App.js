1aaba6eaecf191660e748f5c248fee34c13717ba
Rafael Crempe <rafaelcrempe@gmail.com>
2025-08-14 19:32:20 -0300

    função cadastro

--- projeto_react/src/App.js
+++ projeto_react/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'; //useState permite criar variável, em parceria com função, que faz alterações na tela quando essa variável é alterada
 
+
+const supabaseUrl="https://wvljndxyaidxngxzfmyc.supabase.co"
+const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bGpuZHh5YWlkeG5neHpmbXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTA4NDUsImV4cCI6MjA2OTkyNjg0NX0.KYntjFPUrdxUWrSVdiE4XGmpSn_mRDrsZhEt3JukZB8"
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 function App() { // aqui é JavaScript
 
 
@@ -40,31 +45,31 @@ function App() { // aqui é JavaScript
 
               <label>
                 Nome <br />
-                <input type="text" placeholder="Nome" /><br />
+                <input type="text" placeholder="Nome" onChange={(e) => setUser({ ...user, nome: e.target.value })}/><br />
               </label>
               <label>
                 Sobrenome <br />
-                <input type="text" placeholder="Sobrenome" /><br />
+                <input type="text" placeholder="Sobrenome" onChange={(e) => setUser({ ...user, last_name: e.target.value })}/><br />
               </label>
               <label>
                 Data de Nascimento <br />
-                <input type="date" placeholder="Data Nascimento" /><br />
+                <input type="date" placeholder="Data Nascimento" onChange={(e) => setUser({ ...user, birth: e.target.value })}/><br />
               </label>
               <label>
                 CPF <br />
-                <input type="text" placeholder="CPF" /><br />
+                <input type="text" placeholder="CPF" onChange={(e) => setUser({ ...user, cpf: e.target.value })}/><br />
               </label>
               <label>
                 Telefone <br />
-                <input type="tel" placeholder="Telefone" /><br />
+                <input type="tel" placeholder="Telefone" onChange={(e) => setUser({ ...user, phone: e.target.value })}/><br />
               </label>
               <label>
                 Email <br />
-                <input type="email" placeholder="E-mail" /><br />
+                <input type="email" placeholder="E-mail" onChange={(e) => setUser({ ...user, email: e.target.value })}/><br />
               </label>
               <label>
                 Senha <br />
-                <input type="password" placeholder="Senha" /><br />
+                <input type="password" placeholder="Senha" onChange={(e) => setUser({ ...user, password: e.target.value })}/><br />
               </label>
               <label>
                 Confirmar Senha <br />
