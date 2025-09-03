752c21f01b08574ccd8af643ef82dc8ad0882737
Renan Victorino <renan.gvg@hotmail.com>
2025-09-02 22:03:08 -0300

    criação componente comentado

--- projeto_react/src/Components/Button.js
+++ projeto_react/src/Components/Button.js
@@ -15,3 +15,40 @@ function Button ({
 
     );
 }
+
+
+
+
+// function Form({
+//     func,
+//     title,
+//     children
+
+// }) {
+
+//     return(
+//         <div className='cardForm' >
+//             <h3>{title}</h3>
+//             <form onSubmit={(e) => e.preventDefault() }>
+//             {children}
+
+//             <Button onClick={ () => send() } disabled={loading} >
+//                 {loading? (
+//                     <Spinner animation="border" role="status">
+//                         <span> className="visually-hidden"
+//                             Loading...
+//                         </span>
+//                     </Spinner>
+//                 ):(
+//                     <>salvar</>
+//                 )}
+//                 </Button>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export {Form}
+
+
+
