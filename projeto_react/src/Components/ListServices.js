7c6391606d0a8ab48f0488ec976a446ada8117f2
marcoshenriquecurila <mhcurila@gmail.com>
2025-09-05 21:06:48 -0300

    feito a funcao ListServices

---
+++ projeto_react/src/Components/ListServices.js
@@ -0,0 +1,56 @@
+
+ function ListServices({
+
+
+    services,
+    funcVer,
+    funcEditar,
+
+}){ 
+    
+    return(
+<div>
+{services.map(
+          s => (
+
+            <div key={s.id}>
+            
+      
+              <h1>{s. professional_id}</h1> <button>{s.status}</button>
+              <div style={{ margin: '10px 0' }}>
+                <StarRating rating={s.star} readonly={true} />
+              </div>
+ 
+              <p>{s.review}</p>
+              {/*<Button variant="danger"onclick={() => delServices(s.id)}>Excluir</Button>*/}
+              <Button variant="primary" onClick={funcVer}>Ver</Button>
+              <Button variant="warning" onClick={funcEditar}>Editar</Button>
+            </div>
+            
+
+          )
+      )}
+</div>
+    );
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// {/* <p>{s.review}</p>
+// {/*<Button variant="danger"onclick={() => delServices(s.id)}>Excluir</Button>*/}
+ //<Button variant="primary" onClick={() => nav( `/services/${s.id}`, {replace: true})}>Ver</Button>
+ //<Button variant="warning" onClick={() => nav( `/services/edit/${s.id}`, {replace: true})}>Editar</Button>
+// </div> */}
